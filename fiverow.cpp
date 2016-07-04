7cb4aeb4f78843fd69756a833a583d14d28be68d
Smirnov Denis <denis.smirnov@intel.com>
2016-07-04 08:00:49 +0300

    Improved algorithm

--- fiverow.cpp
+++ fiverow.cpp
@@ -6,23 +6,32 @@ FiveRow::FiveRow() {   }
 
 bool FiveRow::isVictory(char** cellValue, int horizontalSize, int x, int y)
 {
+    // Получение значения по координате (x,y) - 'x' или 'o'
     char ch = cellValue[x][y];
+    // Проверка победы по вертикали
     bool vertical = checkVertical(cellValue, x, y, ch);
+    // Проверка победы по горизонтали
     bool horizontal = checkHorizontal(cellValue, horizontalSize, x, y, ch);
+    // Проверка победы по главной диагонали
     bool mainDiagonal = checkMainDiagonal(cellValue, horizontalSize, x, y, ch);
+    // Проверка победы по побочной диагонали
     bool secondaryDiagonal = checkSecondaryDiagonal(cellValue, horizontalSize, x, y, ch);
+    // Возвращаем true, если хотя бы по одной из линий - победа (true), иначе - false
     return false || vertical || horizontal || mainDiagonal || secondaryDiagonal;
 }
 
 bool FiveRow::checkVertical(char** cellValue, int x, int y, char ch)
 {
+    // Количество одинаковых символов в ряд: ch = 'x' или 'y'
     int victoryCondition = 1;
+    // Движение вверх по вертикали от нажатой ячейки
     int limit = y-5 > -1 ? y-5 : -1;
     for (int i=y-1; i>limit; i--) {
         if (cellValue[x][i] == ch) {
             victoryCondition++;
         } else break;
     }
+    // Движение вниз по вертикали от нажатой ячейки
     int verticalSize = strlen(cellValue[0]);
     limit = y+5 < verticalSize ? y+5 : verticalSize;
     for (int i=y+1; i<limit; i++) {
@@ -38,13 +47,16 @@ bool FiveRow::checkVertical(char** cellValue, int x, int y, char ch)
 
 bool FiveRow::checkHorizontal(char** cellValue, int horizontalSize, int x, int y, char ch)
 {
+    // Количество одинаковых символов в ряд: ch = 'x' или 'y'
     int victoryCondition = 1;
+    // Движение влево по горизонтали от нажатой ячейки
     int limit = x-5 > -1 ? x-5 : -1;
     for (int i=x-1; i>limit; i--) {
         if (cellValue[i][y] == ch) {
             victoryCondition++;
         } else break;
     }
+    // Движение вправо по горизонтали от нажатой ячейки
     limit = x+5 < horizontalSize ? x+5 : horizontalSize;
     for (int i=x+1; i<limit; i++) {
         if (cellValue[i][y] == ch) {
@@ -59,7 +71,9 @@ bool FiveRow::checkHorizontal(char** cellValue, int horizontalSize, int x, int y
 
 bool FiveRow::checkMainDiagonal(char** cellValue, int horizontalSize, int x, int y, char ch)
 {
+    // Количество одинаковых символов в ряд: ch = 'x' или 'y'
     int victoryCondition = 1;
+    // Движение вверх по главной диагонали от нажатой ячейки
     int limit = x+1 < y+1 ? x+1 : y+1;
     limit = limit < 5 ? limit : 5;
     for (int i=1; i<limit; ++i) {
@@ -67,6 +81,7 @@ bool FiveRow::checkMainDiagonal(char** cellValue, int horizontalSize, int x, int
             victoryCondition++;
         } else break;
     }
+    // Движение вниз по главной диагонали от нажатой ячейки
     int verticalSize = strlen(cellValue[0]);
     limit = horizontalSize-x < verticalSize-y ? horizontalSize-x : verticalSize-y;
     limit = limit < 5 ? limit : 5;
@@ -83,7 +98,9 @@ bool FiveRow::checkMainDiagonal(char** cellValue, int horizontalSize, int x, int
 
 bool FiveRow::checkSecondaryDiagonal(char** cellValue, int horizontalSize, int x, int y, char ch)
 {
+    // Количество одинаковых символов в ряд: ch = 'x' или 'y'
     int victoryCondition = 1;
+    // Движение вверх по побочной диагонали от нажатой ячейки
     int limit = horizontalSize-x < y+1 ? horizontalSize-x : y+1;
     limit = limit < 5 ? limit : 5;
     for (int i=1; i<limit; ++i) {
@@ -91,6 +108,7 @@ bool FiveRow::checkSecondaryDiagonal(char** cellValue, int horizontalSize, int x
             victoryCondition++;
         } else break;
     }
+    // Движение вниз по побочной диагонали от нажатой ячейки
     int verticalSize = strlen(cellValue[0]);
     limit = verticalSize-y < x+1 ? verticalSize-y : x+1;
     limit = limit < 5 ? limit : 5;
@@ -109,20 +127,23 @@ int FiveRow::getCellNumber(char **cellValue, int horizontalSize)
 {
     int cellNumber;
     int verticalSize = strlen(cellValue[0]);
+    // Массив уровней ячеек по 'x'
     int *lossLevelCell = new int[horizontalSize*verticalSize];
+    // Массив уровней ячеек по 'o'
     int *winLevelCell = new int[horizontalSize*verticalSize];
+    // Получение массивов уровней ячеек по 'x' и по 'o'
     lossLevelCell = getCellLevelArray(cellValue, horizontalSize, verticalSize, 'x');
     winLevelCell = getCellLevelArray(cellValue, horizontalSize, verticalSize, 'o');
-    int maxNumber = getMax(lossLevelCell, horizontalSize*verticalSize);
-    for (int i=0; i<horizontalSize*verticalSize; ++i) {
-        if(lossLevelCell[i] != maxNumber) {
-            lossLevelCell[i] = -1;
-        }
-        else {
-            lossLevelCell[i] += winLevelCell[i];
-        }
+    // Получение максимального уровня из массива уровней ячеек по 'x' и по 'o'
+    int lossMaxNumber = getMax(lossLevelCell, horizontalSize*verticalSize);
+    int winMaxNumber = getMax(winLevelCell, horizontalSize*verticalSize);
+    // Получение индекса ячейки с максимальным уровнем из массива уровней ячеек по 'x'
+    if (lossMaxNumber > winMaxNumber) {
+        cellNumber = getIndexOfMax(lossLevelCell, horizontalSize*verticalSize);
     }
-    cellNumber = getIndexOfMax(lossLevelCell, horizontalSize*verticalSize);
+    else {
+        cellNumber = getIndexOfMax(winLevelCell, horizontalSize*verticalSize);
+    }
     delete [] lossLevelCell;
     delete [] winLevelCell;
     return cellNumber;
@@ -130,15 +151,20 @@ int FiveRow::getCellNumber(char **cellValue, int horizontalSize)
 
 int* FiveRow::getCellLevelArray(char **cellValue, int horizontalSize, int verticalSize, char ch) {
     int *levelCell = new int[horizontalSize*verticalSize];
+    int levelDirect[4];
     int index = 0;
     for (int i=0; i<horizontalSize; ++i) {
         for (int j=0; j<verticalSize; ++j) {
             if (cellValue[i][j] == ' ') {
-                levelCell[index] = 0;
-                levelCell[index] += verticalLevelCell(cellValue, i, j, ch);
-                levelCell[index] += horizontalLevelCell(cellValue, horizontalSize, i, j, ch);
-                levelCell[index] += mainDiagonalLevelCell(cellValue, horizontalSize, i, j, ch);
-                levelCell[index++] += secondaryDiagonalLevelCell(cellValue, horizontalSize, i, j, ch);
+                // Вычисление и прибавление уровня ячейки по вертикали
+                levelDirect[0] = verticalLevelCell(cellValue, i, j, ch);
+                // Вычисление и прибавление уровня ячейки по горизонтали
+                levelDirect[1] = horizontalLevelCell(cellValue, horizontalSize, i, j, ch);
+                // Вычисление и прибавление уровня ячейки по главной диагонали
+                levelDirect[2] = mainDiagonalLevelCell(cellValue, horizontalSize, i, j, ch);
+                // Вычисление и прибавление уровня ячейки по побочной диагонали
+                levelDirect[3] = secondaryDiagonalLevelCell(cellValue, horizontalSize, i, j, ch);
+                levelCell[index++] = getMax(levelDirect, 4);
             }
             else if (cellValue[i][j] == 'o') {
                 levelCell[index++] = -1;
@@ -214,10 +240,15 @@ int FiveRow::secondaryDiagonalLevelCell(char **cellValue, int horizontalSize, in
 
 int FiveRow::getCellLevel(char *cellValue, int size, int index, char ch)
 {
+    // Результативный уровень ячейки
     int cellLevel = 0;
+    // Количество ячеек для выйгрыша (если >=4, то на этом участке можно выйграть)
     int numFreeCell = 0;
+    // Очки, вычисляемый по расстоянию до первой необходимой ячейки со значением ch = 'x' или 'o' (чем ближе к необходимой ячейке, тем больше очков)
     int distancePoint = 0;
+    // Количество ячеек со значением ch = 'x' или 'o'
     int numNecessaryCell = 0;
+    // Движение влево по массиву cellValue
     int limit = index-5 > -1 ? index-5 : -1;
     for (int i=index-1; i>limit; i--) {
         if (cellValue[i] == ' ') {
@@ -230,6 +261,7 @@ int FiveRow::getCellLevel(char *cellValue, int size, int index, char ch)
         }
         else break;
     }
+    // Движение вправо по массиву cellValue
     limit = index+5 < size ? index+5 : size;
     for (int i=index+1; i<limit; i++) {
         if (cellValue[i] == ' ') {
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,6 +6,7 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    // Инициализация UI элементов
     gameField = new QFrame(this);
     gameField->setFrameStyle(QFrame::Box);
     gameField->setLineWidth(1);
@@ -14,18 +15,24 @@ MainWindow::MainWindow(QWidget *parent) :
     newGameButton = new QPushButton("New Game", this);
     message = new QLabel(this);
 
+    // Начальная отрисовка окна с игровым полем 5x5
     drawWindow(5, 5);
 
+    // Соединение сигнала нажатия кнопки "New Game" с функцией обработки этой кнопки handleNewGameButton()
     connect(newGameButton, SIGNAL(clicked()), this, SLOT (handleNewGameButton()));
+    // Инициализация объекта класса FiveRow
     fr = new FiveRow();
 }
 
 void MainWindow::drawWindow(int horizontalCellNumber, int verticalCellNumber)
 {
+    // Размер игровой ячейки в пикселях
     const int cellSize = 20;
+    // Размеры игрового поля
     int horizontalFieldSize = cellSize*horizontalCellNumber;
     int verticalFieldSize = cellSize*verticalCellNumber;
 
+    // Очистка игрового поля и массива значений игровых ячеек при переотрисовке игрового окна
     if (cellList.length() != 0) {
         for (int i=0; i<cellList.length(); ++i) {
             delete cellList.at(i);
@@ -37,11 +44,13 @@ void MainWindow::drawWindow(int horizontalCellNumber, int verticalCellNumber)
         delete [] cellValue;
     }
 
+    // Выставление начальной координаты и размеров игрового поля
     gameField->setGeometry(QRect(QPoint(10, 20), QSize(horizontalFieldSize+1, verticalFieldSize+1)));
     if (!gameField->isEnabled()) {
         gameField->setEnabled(true);
     }
 
+    // Инициализация  (и добавление их в список cellList) и массива значений игровых ячеек
     QPushButton *cell;
     cellValue = new char* [horizontalCellNumber];
     for (int i=0; i<horizontalCellNumber; ++i) {
@@ -55,6 +64,7 @@ void MainWindow::drawWindow(int horizontalCellNumber, int verticalCellNumber)
         cellValue[i][verticalCellNumber] = '\0';
     }
 
+    // Соединение сигнала нажатия кнопок (ячеек) игрового поля с функцией обработки этих кнопок handleCell(int)
     QSignalMapper* signalMapper = new QSignalMapper(this);
     for (int i=0; i<cellList.length(); ++i) {
         cellList.at(i)->setParent(gameField);
@@ -64,17 +74,22 @@ void MainWindow::drawWindow(int horizontalCellNumber, int verticalCellNumber)
     }
     connect(signalMapper, SIGNAL(mapped(int)), this, SLOT(handleCell(int)));
 
+    // Выставление начальной координаты, размеров и текста для текстового поля ширины
     width->setGeometry(QRect(QPoint(10, verticalFieldSize+30), QSize(41, 31)));
     width->setText(QString::number(horizontalCellNumber));
 
+    // Выставление начальной координаты, размеров и текста для текстового поля высоты
     height->setGeometry(QRect(QPoint(60, verticalFieldSize+30), QSize(41, 31)));
     height->setText(QString::number(verticalCellNumber));
 
+    // Выставление начальной координаты и размеров кнопки "New Game"
     newGameButton->setGeometry(QRect(QPoint(110, verticalFieldSize+30), QSize(61, 31)));
 
+    // Выставление начальной координаты, размеров и текста для сообщения
     message->setGeometry(QRect(QPoint(10, verticalFieldSize+60), QSize(101, 31)));
     message->setText("");
 
+    // Выставление фиксированного размеры для главного окна приложения и его обновление
     int horizontalWindowSize = horizontalFieldSize+21 > 181 ? horizontalFieldSize+21 : 181;
     this->setFixedSize(QSize(horizontalWindowSize, verticalFieldSize+91));
     this->update();
@@ -82,13 +97,16 @@ void MainWindow::drawWindow(int horizontalCellNumber, int verticalCellNumber)
 
 void MainWindow::handleNewGameButton()
 {
+    // Значения, ограничивающие ширину и высоту игрового поля
     int minCellNumber = 5;
     int maxCellNumber = 25;
+    // Получение значений ширины и высоты игрового поля из UI
     int horizontalCellNumber = width->toPlainText().toInt();
     int verticalCellNumber = height->toPlainText().toInt();
     if (horizontalCellNumber >=minCellNumber && horizontalCellNumber <=maxCellNumber &&
         verticalCellNumber >=minCellNumber && verticalCellNumber <=maxCellNumber)
     {
+        // Переотрисовка игрового окна
         drawWindow(horizontalCellNumber, verticalCellNumber);
     }
     else {
@@ -100,27 +118,37 @@ void MainWindow::handleNewGameButton()
 void MainWindow::handleCell(int cellNumber)
 {
     if (!cellList.at(cellNumber)->text().compare(" ")) {
+        // Получение значений ширины и высоты игрового поля из UI
         int horizontalCellNumber = width->toPlainText().toInt();
         int verticalCellNumber = height->toPlainText().toInt();
+        // Остаток от деления для получения индексов элемента двумерного массива игровых ячеек из индекса элемента из списка игровых ячеек
         int remainder = cellNumber%verticalCellNumber;
+        // Установка "x" в нажатой ячейке
         cellList.at(cellNumber)->setText("x");
+        // Изменение соответствующего значения элемента двумерного массива игровых ячеек на 'x'
         cellValue[(cellNumber-remainder)/verticalCellNumber][remainder] = 'x';
+        // Проверка на победу 'x'
         if (fr->isVictory(cellValue, horizontalCellNumber, (cellNumber-remainder)/verticalCellNumber, remainder)) {
             cellList.at(cellNumber)->setStyleSheet("font: bold 14px");
             message->setText("You win!");
             gameField->setEnabled(false);
         }
+        // Проверка на существование хода
         else if (fr->existTurn(cellValue, horizontalCellNumber)) {
+            // Получение индекса элемента из списка игровых ячеек для хода компьютера
             int computerCellNumber = fr->getCellNumber(cellValue, horizontalCellNumber);
+            // Ход компьютера
             remainder = computerCellNumber%verticalCellNumber;
             cellList.at(computerCellNumber)->setText("o");
             cellValue[(computerCellNumber-remainder)/verticalCellNumber][remainder] = 'o';
+            // Проверка на победу 'o'
             if (fr->isVictory(cellValue, horizontalCellNumber, (computerCellNumber-remainder)/verticalCellNumber, remainder)) {
                 cellList.at(computerCellNumber)->setStyleSheet("font: bold 14px");
                 message->setText("You lose!");
                 gameField->setEnabled(false);
             }
         }
+        // Ничья (все ячейки заполнены)
         else {
             message->setText("Dead head!");
             gameField->setEnabled(false);
@@ -130,6 +158,7 @@ void MainWindow::handleCell(int cellNumber)
 
 MainWindow::~MainWindow()
 {
+    // Очистка игрового поля
     for (int i=0; i<cellList.length()/height->toPlainText().toInt(); ++i) {
         delete [] cellValue[i];
     }
